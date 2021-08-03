6f25a19f437bc5649f2e30145273c0282f49925a
Nabil Babu <nabil.babu20@gmail.com>
2021-08-02 23:12:54 -0400

    Basic React App using useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import AddToList from "./components/AddToList";
+import List from "./components/List";
+
+export interface IState {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
+}
 
 function App() {
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "Nabil Babu",
+      age: 30,
+      url: "https://media-exp1.licdn.com/dms/image/C4E03AQEgcfP2e2r-kA/profile-displayphoto-shrink_200_200/0/1603370850005?e=1632960000&v=beta&t=RaZmT38IElEU-wSWTaCBvlH_-1aXbV1qH1aBuf7zPoM",
+      note: "hot af",
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People Invited to my Party</h1>
+      <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
     </div>
   );
 }
